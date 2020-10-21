f3dddf4335228b56dbed64af7410b848e191f6f1
ryan.attard <ryan.attard@mcast.edu.mt>
2020-10-21 13:54:51 +0200

    Initial Commit - Creatng website

---
+++ SWD62AEP/Presentation/Controllers/SupportController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class SupportController : Controller
+    {
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
