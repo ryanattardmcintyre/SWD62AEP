db908f7b238b3ac8edb17e2bc973df255a4739d5
ryan.attard <ryan.attard@mcast.edu.mt>
2020-10-26 09:28:28 +0100

    Creating the basis for our clean architecture

--- SWD62AEP/Presentation/Controllers/SupportController.cs
+++ SWD62AEP/Presentation/Controllers/SupportController.cs
@@ -8,9 +8,30 @@ namespace Presentation.Controllers
 {
     public class SupportController : Controller
     {
+        //this method will be called using a get request and its purpose is to load the page
+        [HttpGet]
         public IActionResult Contact()
-        {
+        {  
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Contact(string email, string query)
+        {
+            //ViewData, ViewBag
+            if(string.IsNullOrEmpty(query))
+            {
+                ViewData["warning"] = "Question was left empty";
+            }
+            else
+            {
+                ViewData["feedback"] = "Thank you for your query, we will get back to you asap";
+            }
+            //ViewBag.feedback = "";
+
+            return View();
+        }
+
+
     }
 }
