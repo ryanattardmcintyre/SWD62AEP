c14eba51267f99622c92057461a18ac95283c42a
ryan.attard <ryan.attard@mcast.edu.mt>
2020-12-16 13:30:41 +0100

    Assignment hints

---
+++ SWD62AEP/ShoppingCart.Domain/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Domain.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+
+        public string Email { get; set; }
+        public DateTime DatePlaced { get; set; }
+
+    }
+}
